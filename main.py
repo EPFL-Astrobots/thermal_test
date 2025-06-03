a26927af8efe8078dcc9179c30bf38e94b1bd088
MaximeRombach <rombach.maxime@gmail.com>
2025-06-03 15:41:07 +0200

    timestamped log files

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from collections import deque
 # Config
 ROLLING_WINDOW_SECONDS = 300  # Show only last 5 minutes
 PORT = 'COM3'
-LOG_FILE = 'chamber_log.csv'
+NOW_TIME = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+LOG_FILE = NOW_TIME + 'chamber_log.csv'
 MAX_POINTS = 100  # max points in graph
 start_time = time.time()
 
