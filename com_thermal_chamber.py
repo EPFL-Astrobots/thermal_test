5280a2063633a2ff42d9c87bc81dc7bd0087d2c8
MaximeRombach <73754365+MaximeRombach@users.noreply.github.com>
2025-06-03 13:20:20 +0200

    First script to talk to the thermal chamber
    Just poll temperature and humidity data

---
+++ com_thermal_chamber.py
@@ -0,0 +1,21 @@
+import serial
+
+ser = serial.Serial(
+    port='COM3',        # Change to your port
+    baudrate=9600,      # Match the chamber setting
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=1           # seconds
+)
+# 
+
+ser.write(b'$'+ b'00I'+b'\r')  # Command to read temperature
+response = ser.readline()
+fields = response.decode(errors='ignore').strip().split(' ')
+# print('Response:', response.decode(errors='ignore').strip().split(','))
+print(f'Temperature setpoint: {float(fields[0])} °C')
+print(f'Temperature current: {float(fields[1])} °C')
+print(f'Humidity setpoint: {float(fields[2])} %')
+print(f'Humidity current: {float(fields[3])} %')
+ser.close()
